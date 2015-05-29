64c1d3f0da81580ada807b11885391ed2ae67fc0
Will Thomas <will306@oneverni.com>
2015-05-29 19:00:57 -0400

    Working lookup, sorting, freq. Wildcards almost work

--- anagram.py
+++ anagram.py
@@ -1,4 +1,4 @@
-import sys, os
+import sys, os, readline
 
 # path = '/Users/willt/Dropbox/Programming/anagram/scowl-2015.05.18/final/american-words.10'
 path = 'american-words.txt'
@@ -6,6 +6,14 @@ path = 'american-words.txt'
 global nodeCount, wordCount
 nodeCount = 0
 wordCount = 0
+
+class Word:
+    def __init__(self, word, frequency, subcategory):
+        self.word = word
+        self.frequency = frequency
+        self.subcategory = subcategory
+    def __str__(self):
+        return '%i %s (%s)' % (self.frequency, self.word, self.subcategory)
 
 class TrieNode(object):
     def __init__(self):
@@ -21,8 +29,12 @@ class TrieNode(object):
             return n
         else:
             return self.children[char]
-    def addWord(self, word):
-        self.words.append(word)
+    def getChildren(self):
+        return self.children.values()
+    def addWord(self, word, frequency, subcategory):
+        self.words.append(Word(word, frequency, subcategory))
+    def getWords(self):
+        return self.words
     def toString(self, indent = 0):
         s = "{ "
         # first = True
@@ -36,23 +48,83 @@ class TrieNode(object):
             s += " " + word
         return s
 
-def buildTrie():
-    rootNode = TrieNode()
-    f = open(path, 'r')
+def wordToChars(word):
+    word = word.strip().strip('.').lower()
+    # chars = [x for x in word]
+    chars = list(word)
+    chars.sort() #What about sorting by character frequency?
+    return chars
+
+def addFileToTrie(rootNode, filePath, frequency, subcategory):
+    f = open(filePath, 'r')
     for line in f:
         node = rootNode
-        word = line.strip()
-        if '\'' in word:
+        if '\'' in line:
             continue
-        for char in word:
+        word = line.strip()
+        for char in wordToChars(word):
             node = node.getChild(char)
-        node.addWord(word)
+        node.addWord(word, frequency, subcategory)
         global wordCount
         wordCount += 1
     return rootNode
 
-def lookup(rootNode):
-    pass
+def buildTrie():
+    rootNode = TrieNode()
+    sizes = [ 10, 20, 35, 40, 50, 55, 60, 70, 80, 95 ]
+    max_size = 10
+    category = 'american' #canadian, british, british_z
+    subcategories = [
+        # 'abbreviations',
+        # 'contractions',
+        # 'proper-names',
+        # 'upper',
+        'words',
+    ]
+    for size in sizes:
+        if size > max_size:
+            break
+        for subcategory in subcategories:
+            addFileToTrie(rootNode,
+                'scowl_word_lists/english-%s.%i' % (subcategory, size),
+                size, subcategory)
+            addFileToTrie(rootNode,
+                'scowl_word_lists/%s-%s.%i' % (category, subcategory, size),
+                size, subcategory)
+    return rootNode
 
-print buildTrie().toString()
+def doLookup(node, chars, wilds):
+    for word in node.getWords():
+        print word
+
+    if len(chars) == 0:
+        return
+
+    if wilds > 0:
+        for n in node.getChildren():
+            doLookup(n, list(chars), wilds - 1)
+
+    char = chars.pop()
+    node = node.getChild(char)
+    doLookup(node, chars, wilds)
+
+def lookup(rootNode, searchWord):
+    print 'Looking up "%s"...' % (searchWord)
+    wilds = 0
+    for char in searchWord:
+        if char == '.':
+            wilds += 1
+    chars = wordToChars(searchWord)
+    chars.reverse()
+    doLookup(rootNode, chars, wilds)
+
+trie = buildTrie()
+# print trie.toString()
 print "Built %i nodes for %i words" % (nodeCount, wordCount)
+
+while True:
+    try:
+        searchWord = raw_input('Search input: ')
+    except EOFError as e:
+        break
+    lookup(trie, searchWord)
