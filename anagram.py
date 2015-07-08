5ed6d48c6dacac484b6635555c6811eb9174ec52
Will Thomas <will306@oneverni.com>
2015-07-08 07:20:57 -0400

    Refactor doLookup to return found word sets
    Previously it would just modify a set passed in as an argument.

--- anagram.py
+++ anagram.py
@@ -22,21 +22,21 @@ def combineSortChars(x, y):
         outputNode = trie.CharNode(char, outputNode)
     return outputNode
 
-def doLookup(rootNode, node, charNode, skippedNode, wilds, foundWordSets, currentWordSets, depth):
+def doLookup(rootNode, node, charNode, skippedNode, wilds, currentWordSets, depth):
     originalNode = node
     remainingStr = ""
     skippedStr = ""
+    foundWordSets = set()
     if config.Settings.memoize:
-        remainingStr = str(charNode)
-        skippedStr = str(skippedNode)
+        remainingStr = combineSortChars(charNode, skippedNode)
         for memo in originalNode.memoizedLookups:
-            if remainingStr == memo.remainingStr and skippedStr == memo.skippedStr and memo.wilds == wilds:
+            if remainingStr == memo.remainingStr and memo.wilds == wilds:
                 for cws in currentWordSets:
                     for mws in memo.wordSets:
-                        fuWordSet = cws | mws
+                        fullWordSet = cws | mws
                         if len(fullWordSet) > 0:
                             foundWordSets.add(fullWordSet)
-                return #Early return since it's memoized
+                return frozenset(foundWordSets)
 
     if node and node.words:
         newCurrentWordSets = []
@@ -45,13 +45,12 @@ def doLookup(rootNode, node, charNode, skippedNode, wilds, foundWordSets, curren
             for word in node.words:
                 newCurrentWordSets.append(cws.union([word]))
                 if config.Settings.traceLookup: print "%sFound: %s (already %s), continuing with %s" % (depth*" ", word, cws, newCharNode)
-        doLookup(rootNode, rootNode, newCharNode, None, wilds, foundWordSets, newCurrentWordSets, depth+1)
-        # currentWordSets = newCurrentWordSets
+        foundWordSets.update(doLookup(rootNode, rootNode, newCharNode, None, wilds, newCurrentWordSets, depth+1))
 
     if wilds > 0:
         if Config.traceLookup: print "%sUsing a wildcard to search all children..." % (depth*" ")
         for n in node.children:
-            doLookup(rootNode, n, charNode, skippedNode, wilds-1, foundWordSets, currentWordSets, depth+1)
+            foundWordSets.update(doLookup(rootNode, n, charNode, skippedNode, wilds-1, currentWordSets, depth+1))
 
     if node == None or charNode == None:
         if config.Settings.traceLookup: print "%sNo characters or children left, terminating with %i word sets" % (depth*" ", len(currentWordSets))
@@ -63,20 +62,20 @@ def doLookup(rootNode, node, charNode, skippedNode, wilds, foundWordSets, curren
         charNode = charNode.next
 
         if config.Settings.traceLookup: print "%sSearching by skipping %s, %s left, %s skipped" % (depth*" ", char, charNode, CharNode(char, skippedNode))
-        doLookup(rootNode, node, charNode, trie.CharNode(char, skippedNode), wilds, foundWordSets, currentWordSets, depth+1)
+        foundWordSets.update(doLookup(rootNode, node, charNode, trie.CharNode(char, skippedNode), wilds, currentWordSets, depth+1))
 
         node = node.getChild(char)
         if node:
             if config.Settings.traceLookup: print "%sSearching by using %s. %s left, %s skipped" % (depth*" ", char, charNode, skippedNode)
-            doLookup(rootNode, node, charNode, skippedNode, wilds, foundWordSets, currentWordSets, depth+1)
+            foundWordSets.update(doLookup(rootNode, node, charNode, skippedNode, wilds, currentWordSets, depth+1))
 
     if config.Settings.memoize:
-        originalNode.memoizedLookups.append(MemorizedLookup(remainingStr, skippedStr, wilds, currentWordSets))
+        originalNode.memoizedLookups.append(MemorizedLookup(remainingStr, wilds, foundWordSets))
+    return foundWordSets
 
 class MemorizedLookup:
-    def __init__(self, remainingStr, skippedStr, wilds, wordSets):
+    def __init__(self, remainingStr, wilds, wordSets):
         self.remainingStr = remainingStr
-        self.skippedStr = skippedStr
         self.wilds = wilds
         self.wordSets = wordSets
 
@@ -91,8 +90,7 @@ def lookup(rootNode, searchWord):
     reversedChars.reverse()
     for ch in reversedChars:
         chars = trie.CharNode(ch, chars)
-    foundWordSets = set()
-    doLookup(rootNode, rootNode, chars, None, wilds, foundWordSets, [frozenset()], 0)
+    foundWordSets = doLookup(rootNode, rootNode, chars, None, wilds, [frozenset()], 0)
     if config.Settings.memoize:
         rootNode.clearMemoized()
     return foundWordSets
@@ -101,7 +99,7 @@ def printWords(foundWordSets, expectedLength):
     l = list(foundWordSets)
     l.sort(key=calcWordSetWeight, reverse=True)
     for wordSet in l:
-        if not Config.includePartialMatches and calcWordSetLength(wordSet) < expectedLength:
+        if not config.Settings.includePartialMatches and calcWordSetLength(wordSet) < expectedLength:
             continue
         print "%i:" % calcWordSetWeight(wordSet),
         for word in wordSet:
@@ -117,19 +115,19 @@ if __name__ == '__main__':
     grp.add_argument("-i", "--interactiveSearch", action='store_true')
     args = parser.parse_args()
 
-    Config.includePartialMatches = args.includePartialMatches
+    config.Settings.includePartialMatches = args.includePartialMatches
 
-    trie = trie.buildTrie()
-    print "Built %i nodes for %i words" % (Stats.nodeCount, Stats.wordCount)
+    trieRoot = trie.buildTrie()
+    print "Built %i nodes for %i words" % (config.Stats.nodeCount, config.Stats.wordCount)
 
     if args.dump:
-        print trie.toString()
+        print trieRoot.toString()
     elif args.search:
-        printWords(lookup(trie, args.search), len(args.search))
+        printWords(lookup(trieRoot, args.search), len(args.search))
     elif args.interactiveSearch:
         while True:
             try:
                 searchWord = raw_input('Search input: ')
             except EOFError as e:
                 break
-            printWords(lookup(trie, searchWord), len(searchWord))
+            printWords(lookup(trieRoot, searchWord), len(searchWord))
